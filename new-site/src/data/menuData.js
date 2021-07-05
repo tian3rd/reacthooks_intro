b752fb95df520cb7e8d91daf05bf465a60ceb7af
tian <tianwu@outlook.com>
2021-07-05 19:15:17 +1000

    refactor data array to a file

---
+++ new-site/src/data/menuData.js
@@ -0,0 +1,27 @@
+export const menuData = [
+  {
+    title: "Courses",
+    icon: "/images/icons-part/courses.svg",
+    link: "/courses",
+  },
+  {
+    title: "Tutorials",
+    icon: "/images/icons-part/tutorials.svg",
+    link: "/tutorial",
+  },
+  {
+    title: "Pricing",
+    icon: "/images/icons-part/pricing.svg",
+    link: "/pricing",
+  },
+  {
+    title: "",
+    icon: "/images/icons-part/search.svg",
+    link: "/search",
+  },
+  {
+    title: "",
+    icon: "/images/icons-part/account.svg",
+    link: "/account",
+  },
+]
