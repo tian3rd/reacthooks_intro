ede8a7b884d4f2565340163caa8bcfe5b02598ee
tian <tianwu@outlook.com>
2021-07-05 22:47:25 +1000

    add keyframe animation

--- new-site/src/components/sections/HeroSection.js
+++ new-site/src/components/sections/HeroSection.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import MockupAnimation from "../animations/MockupAnimation"
 import PurchaseButton from "../buttons/PurchaseButton"
 import { themes } from "../styles/ColorStyles"
@@ -35,6 +35,14 @@ function HeroSection() {
 
 export default HeroSection
 
+// add keyframes as animation effect from styled-components
+const animation = keyframes`
+  /* start point and end point */
+  from { opacity: 0; transform: translateY(-10px); filter: blur(10px);}
+  /* 'from' is equal to 0%; 'to' is equal to 100% */
+  to { opacity: 1; transform: translateY(0px); filter: blur(0px); }
+`
+
 const Wrapper = styled.div`
   /* background: linear-gradient(180deg, #4316db 0%, #9076e7 100%); */
   /* forbid the horizontal scrolling when position absolute overflows */
@@ -54,6 +62,26 @@ const TextWrapper = styled.div`
   max-width: 360px;
   display: grid;
   gap: 30px;
+
+  /* use '>' to '*' to select only immediate children, not children of children */
+  > * {
+    /* use just defined animation with $() */
+    /* set the opacity to 0 first, forwards to stay visible: opacity 1 */
+    opacity: 0;
+    animation: ${animation} 1s forwards;
+
+    /* set delay separately after general animation settings */
+    :nth-child(1) {
+      /* select the 1st child: title */
+      animation-delay: 0s;
+    }
+    :nth-child(2) {
+      animation-delay: 0.2s;
+    }
+    :nth-child(3) {
+      animation-delay: 0.4s;
+    }
+  }
 `
 
 const Title = styled(H1)`
